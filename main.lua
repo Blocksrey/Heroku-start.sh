0e3bf610bae254b8c201dadaf46333f7aa84d5e7
Jeff <jeff@blocks.rocks>
2021-03-03 19:09:21 -0800

    curl

---
+++ main.lua
@@ -0,0 +1,16 @@
+local ffi = require("ffi")
+
+local libopus = ffi.load("libopus.so")
+--[[
+local libcurl = require("curl")
+
+local curl = libcurl.init()
+curl:perform {
+  url = 'http://example.com',
+  postfields = { a = 1, b = 2 }
+}
+
+info = curl:info()
+print('Response code:', info.response_code)
+print('Content type:',  info.content_type)
+--]]
